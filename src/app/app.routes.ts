6274296ebf6584886022456b7dca66e61626e30b
shuhag <shuhag200113@gmail.com>
2025-07-09 14:57:19 +0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login/login';
+import { Dashboard } from './dashboard/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Login},
+    {path:'dashboard',component:Dashboard}
+];
